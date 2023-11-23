53acf1f7191b03f65a4bd1987cf645adec26dde0
Vũ Huy Hoàng <115575325+HiraDuong@users.noreply.github.com>
2023-11-23 21:22:58 +0700

    #

---
+++ Scanner/Scanner/reader.h
@@ -0,0 +1,17 @@
+/* 
+ * @copyright (c) 2008, Hedspi, Hanoi University of Technology
+ * @author Huu-Duc Nguyen
+ * @version 1.0
+ */
+
+#ifndef __READER_H__
+#define __READER_H__
+
+#define IO_ERROR 0
+#define IO_SUCCESS 1
+
+int readChar(void);
+openInputStream();
+void closeInputStream(void);
+
+#endif
